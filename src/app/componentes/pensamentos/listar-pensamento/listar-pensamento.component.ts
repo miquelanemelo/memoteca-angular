e6d346fed5a032ada5d79e299db30d924f300832
Miquelane Melo <miquelanepmelo@gmail.com>
2022-12-30 09:22:58 -0300

    adicionando componente dinamico para pensamentos

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class ListarPensamentoComponent {
 
+  listaPensamentos = [
+    {
+      conteudo: 'Passo informações para o componente filho',
+      autoria: 'Componente pai',
+      modelo: 'modelo3'
+    },
+    {
+      conteudo: 'Minha propriedade é decorada com @Input()',
+      autoria: 'Componente filho',
+      modelo: 'modelo3'
+    },
+    {
+      conteudo: 'O homem que não valoriza a si mesmo, não pode valorizar nada nem ninguém. As pessoas acham que o mentiroso triunfa sobre suas vítimas. O que aprendi é que uma mentira é um ato de autoabdicação, porque quem mente entrega sua realidade à pessoa para quem a mentira se dirige, tornando-se servo daquele indivíduo, ficando condenado dali em diante a falsear a realidade tal qual ela exige. E, ainda que se consiga atingir o objetivo imediato visado pela mentira, o preço que se paga é a destruição daquilo que se pretendia obter. O homem que mente para o mundo é escravo do mundo dali em diante.',
+      autoria: 'Ayn Rand',
+      modelo: 'modelo1'
+    }
+  ];
 }
